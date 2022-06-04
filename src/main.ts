6fbd93b50a184a51fef2f45946892a0e69f4adf9
dmt <dmt@completelydifferent.com>
2022-06-03 21:10:48 -0700

    formatting and try random strokes

--- src/main.ts
+++ src/main.ts
@@ -12,21 +12,24 @@ const params = {
   canvasAspect: 8.5 / 11,
 };
 
-function setCanvasSize(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+function setCanvasSize(
+  canvas: HTMLCanvasElement,
+  context: CanvasRenderingContext2D
+) {
   const windowAspect = window.innerWidth / window.innerHeight;
   let w = window.innerWidth;
   let h = window.innerHeight;
   if (params.canvasAspect > windowAspect) {
     canvas.width = w;
-    h = w/params.canvasAspect;
+    h = w / params.canvasAspect;
     canvas.height = h;
   } else {
     canvas.height = h;
-    w = h*params.canvasAspect;
+    w = h * params.canvasAspect;
     canvas.width = w;
   }
   // set coordinate system to 0,0-1,1
-  context.setTransform(w,0,0, h, 0,0);
+  context.setTransform(w, 0, 0, h, 0, 0);
   //context.scale(canvas.width, canvas.height);
   context.lineWidth = 0.001;
 }
@@ -54,14 +57,14 @@ const winput = pane.addInput(params, "width", {
   max: 1,
   step: 0.001,
 });
-winput.on('change', () => {
+winput.on("change", () => {
   setCurveSize(params.width);
 });
 
 const btn = pane.addButton({
-  title: 'Pause/Play',
+  title: "Pause/Play",
 });
-btn.on('click', () => {
+btn.on("click", () => {
   params.isDrawing = !params.isDrawing;
   if (params.isDrawing) {
     window.requestAnimationFrame(render);
@@ -132,6 +135,7 @@ function triple(
 }
 
 function render(t: DOMHighResTimeStamp) {
+  const oldlw = ctx.lineWidth;
 
   ctx.translate(0.2, 0.2);
   straight(ctx);
@@ -145,13 +149,29 @@ function render(t: DOMHighResTimeStamp) {
 
   ctx.translate(0.6, 0.2);
   triad(ctx, straight, 0.02, 0);
-  ctx.setTransform(canvas.width,0,0, canvas.height, 0,0);
+  ctx.translate(-0.6, -0.2);
+  ctx.setTransform(canvas.width, 0, 0, canvas.height, 0, 0);
   ctx.stroke();
 
   ctx.translate(0.8, 0.2);
   triad(ctx, curvy1, 0.02, 0);
-  ctx.setTransform(canvas.width,0,0, canvas.height, 0,0);
+  ctx.translate(-0.8, -0.2);
+  ctx.setTransform(canvas.width, 0, 0, canvas.height, 0, 0);
   ctx.stroke();
+
+  const x = rand(0.02, 0.98);
+  const y = rand(0.02, 0.98);
+  const ang = rand(-4, 4);
+  const linewidth = y * 0.001; //rand(0.01, 0.1);
+  ctx.translate(x, y);
+  ctx.rotate((ang * Math.PI) / 180);
+  ctx.lineWidth = linewidth;
+  triad(ctx, curvy1, 0.02, 0);
+  ctx.rotate((-ang * Math.PI) / 180);
+  ctx.translate(-x, -y);
+  ctx.setTransform(canvas.width, 0, 0, canvas.height, 0, 0);
+  ctx.stroke();
+  ctx.lineWidth = oldlw;
 
   if (params.isDrawing) {
     window.requestAnimationFrame(render);
--- src/strokes.ts
+++ src/strokes.ts
@@ -1,29 +1,41 @@
 let curvesize = 0.01;
 
-export function setCurveSize(size:number) {
-    curvesize = size;
+export function setCurveSize(size: number) {
+  curvesize = size;
 }
 
-export function straight(ctx:CanvasRenderingContext2D) {
-    ctx.moveTo(0,-curvesize);
-    ctx.lineTo(0, curvesize);
-    //ctx.moveTo(0,0);
+export function straight(ctx: CanvasRenderingContext2D) {
+  ctx.moveTo(0, -curvesize);
+  ctx.lineTo(0, curvesize);
+  //ctx.moveTo(0,0);
 }
 
-export function curvy1(ctx:CanvasRenderingContext2D) {
-    ctx.moveTo(-0.25*curvesize, -curvesize);
-    ctx.bezierCurveTo(0.25*curvesize, -0.333*curvesize, 0.25*curvesize, 0.333*curvesize, -0.25*curvesize, curvesize);
-    //ctx.moveTo(0,0);
+export function curvy1(ctx: CanvasRenderingContext2D) {
+  ctx.moveTo(-0.25 * curvesize, -curvesize);
+  ctx.bezierCurveTo(
+    0.25 * curvesize,
+    -0.333 * curvesize,
+    0.25 * curvesize,
+    0.333 * curvesize,
+    -0.25 * curvesize,
+    curvesize
+  );
+  //ctx.moveTo(0,0);
 }
 
-export function triad(ctx:CanvasRenderingContext2D, strokeFn:(ctx:CanvasRenderingContext2D)=>void, dx:number, dy:number) {
-    // ctx.beginPath();
-    ctx.translate(-dx, -dy);
-    strokeFn(ctx);
-    ctx.translate(dx, dy);
-    strokeFn(ctx);
-    ctx.translate(dx, dy);
-    strokeFn(ctx);
-    ctx.moveTo(0,0);
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
+export function triad(
+  ctx: CanvasRenderingContext2D,
+  strokeFn: (ctx: CanvasRenderingContext2D) => void,
+  dx: number,
+  dy: number
+) {
+  // ctx.beginPath();
+  ctx.translate(-dx, -dy);
+  strokeFn(ctx);
+  ctx.translate(dx, dy);
+  strokeFn(ctx);
+  ctx.translate(dx, dy);
+  strokeFn(ctx);
+  ctx.moveTo(0, 0);
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
 }
